cc074346e34c812fe471a358c225adc43738a458
shilibadii <shilibadii86@gmail.com>
2024-12-27 17:37:13 +0100

    finish

--- main.py
+++
@@ -1,6 +0,0 @@
-import os
-import pandas as pd
-import tensorflow as tf
-import numpy as np
-df = pd.read_csv(os.path.join('jigsaw-toxic-comment-classification-challenge','train.csv', 'train.csv'))
-df.head()
